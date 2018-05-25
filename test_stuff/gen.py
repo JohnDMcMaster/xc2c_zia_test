3c5a162dbcee1269e7faaae43372a8a94db1bfca
John McMaster <johndmcmaster@gmail.com>
2018-05-25 16:48:28 -0700

    Clean up generation
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- test_stuff/gen.py
+++ test_stuff/gen.py
@@ -24,37 +24,80 @@ import random
 
 random.seed(0)
 
+def randnot():
+    return random.choice(['', '!'])
+
 # last one that doesn't completly explode
 if 1:
     nin = 40
     nout = 16
 
+# External inputs
+FB_I = 40
+# All PLA inputs (includes I/O / FF)
+FB_IA = 56
+# Outputs
+FB_O = 16
 
-print '`timescale 1ns / 1ps'
-print 'module top('
+def module_header(name, pins):
+    print 'module %s(' % name
+    for pi, (pdir, name) in enumerate(pins):
+        if pi != len(pins) - 1:
+            eol = ','
+        else:
+            eol = ');'
+        print '        %s wire %s%s' % (pdir, name, eol)
 
-for i in xrange(nin):
-    print '    input wire myin%d,' % i
+def module_connect(module_name, instance_name, pins):
+    print '    %s %s(' % (module_name, instance_name)
+    for pi, (pin_name, net_name) in enumerate(pins):
+        if pi != len(pins) - 1:
+            eol = ','
+        else:
+            eol = ');'
+        print '            .%s(%s)%s' % (pin_name, net_name, eol)
 
-for i in xrange(nout):
-    print '    output wire myout%d_out' % i,
-    if i != nout - 1:
-        print ','
-    else:
-        print
-print '    );'
-print
+def gen_fb_module(fbn):
+    module_name = 'my_FB%d' % fbn
+    ipins = [('input', 'in_%d' % i) for i in xrange(FB_I)]
+    opins = [('output', 'out_%d' % i) for i in xrange(FB_O)]
+    module_header(module_name, ipins + opins)
 
-def randnot():
-    return random.choice(['', '!'])
+    print
 
-for i in xrange(nout):
-	print '    (* LOC="FB2" *) wire myout%d;' % (i)
-	print '    assign myout%d_out = myout%d;' % (i, i)
+    for i in xrange(nout):
+        # XXX: think can combine these two lines 
+	    print '    (* LOC="FB%d", keep="true", DONT_TOUCH="true" *) wire loc_out_%d;' % (fbn, i)
+	    print '    assign out_%d = loc_out_%d;' % (i, i)
 
-for i in xrange(nout):
-	# assign myout1 = myin1 & myin2 & !myin3 & !myin4;
-	print '    assign myout%d = %s;' % (i, ' & '.join([randnot() + 'myin%d' % i for i in xrange(nin)]))
+    print
 
-print 'endmodule'
+    for i in xrange(nout):
+	    # assign myout1 = myin1 & myin2 & !myin3 & !myin4;
+	    print '    assign loc_out_%d = %s;' % (i, ' & '.join([randnot() + 'in_%d' % i for i in xrange(nin)]))
+    print 'endmodule'
+
+def fb_inout(fbn):
+    ipins = [('in_%d' % i, 'in_%d' % i) for i in xrange(FB_I)]
+    opins = [('out_%d' % i, 'out_%d' % i) for i in xrange(FB_O)]
+    module_connect('my_FB%s' % fbn, 'fb%d' % fbn,
+        ipins + opins)
+
+def run():
+    print '`timescale 1ns / 1ps'
+    print
+    # top
+    module_name = 'top'
+    ipins = [('input', 'in_%d' % i) for i in xrange(FB_I)]
+    opins = [('output', 'out_%d' % i) for i in xrange(FB_O)]
+    module_header(module_name, ipins + opins)
+
+    print
+
+    fb_inout(2)
+    print 'endmodule'
+
+    print
+    gen_fb_module(2)
+run()
 
